5e213c57afe65b62c502b2236dd74ec4ef84e6d3
EduardoJSM <eduu.macedo109@gmail.com>
2026-09-20 17:14:40 -0300

    feat: integrar Analytics e SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -31,6 +32,7 @@ export default function RootLayout({
       <body className="antialiased bg-[#FBFBFB] text-zinc-900 selection:bg-[#77216F] selection:text-white">
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
